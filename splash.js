4efef595f304323a7481062acd363a1ab35cbce8
suspiciousbanana <78773460+spinningbanana@users.noreply.github.com>
2021-05-12 21:01:49 -0700

    add changing text
    changed the text in the bottom right to change every time you reload the page instead of it saying "cool website" all the time

---
+++ splash.js
@@ -0,0 +1,67 @@
+var quotes=new Array()
+
+/* higher chance to say "cool website" */
+quotes[14]='cool website'
+quotes[15]='cool website'
+quotes[16]='cool website'
+quotes[17]='cool website'
+quotes[18]='cool website'
+quotes[19]='cool website'
+quotes[20]='cool website'
+quotes[25]='cool website'
+quotes[26]='cool website'
+
+/* splashes */
+quotes[0]='ding'
+
+quotes[1]='website cool'
+
+quotes[2]='cool epic website that is epic (cool)'
+
+quotes[3]='amog us'
+
+quotes[4]='now with 0 advertising!'
+
+quotes[5]='pico funny'
+
+quotes[6]='the'
+
+quotes[7]='"shit" -Obama'
+
+quotes[8]='check out <a href="https://octaviomaury.github.io/">this</a> website, too!'
+
+quotes[9]='fun fact: uhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+
+quotes[10]='andy'
+
+quotes[11]='yea'
+
+quotes[12]='scary!!!'
+
+quotes[13]='"ILL KILL YOU!!!!!!" -octavo mary'
+
+/* skip 7 splashes */
+
+quotes[21]='i spent way too much time on this'
+
+quotes[22]='check out the games!'
+
+quotes[23]='computer'
+
+quotes[24]='sus!'
+
+/* skip 2 splashes */
+
+quotes[27]='So... * chunks, huh?'
+
+quotes[28]='this is splash text #28'
+
+quotes[29]='You.'
+
+quotes[30]='am i cool now?'
+
+/* code stuffs */
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
+
+/* script by JavaScript Kit http://javascriptkit.com */
