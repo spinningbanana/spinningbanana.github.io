8efecb3106ba64b6f6d4254c157af99e23b056d0
suspiciousbanana <78773460+spinningbanana@users.noreply.github.com>
2021-05-16 12:19:46 -0700

    Update splash.js
    added more splashes + even higher chance to display 'cool website'

--- splash.js
+++ splash.js
@@ -7,9 +7,20 @@ quotes[16]='cool website'
 quotes[17]='cool website'
 quotes[18]='cool website'
 quotes[19]='cool website'
+
 quotes[20]='cool website'
 quotes[25]='cool website'
 quotes[26]='cool website'
+
+quotes[35]='cool website'
+quotes[36]='cool website'
+quotes[37]='cool website'
+quotes[38]='cool website'
+quotes[39]='cool website'
+quotes[40]='cool website'
+quotes[41]='cool website'
+quotes[42]='cool website'
+quotes[43]='cool website'
 
 /* splashes */
 quotes[0]='ding'
@@ -59,6 +70,26 @@ quotes[28]='this is splash text #28'
 quotes[29]='You.'
 
 quotes[30]='am i cool now?'
+
+quotes[31]='where is my wife'
+
+quotes[32]='sample text'
+
+quotes[33]='10 kills on the board right now'
+
+quotes[34]='bana'
+
+/* skip 9 splashes */
+
+quotes[44]='mcdoanld'
+
+quotes[45]='this is splash text #45'
+
+quotes[46]='um...............................'
+
+quotes[47]='yknow..? im like,,, a little random.??'
+
+quotes[48]='bussin'
 
 /* code stuffs */
 var whichquote=Math.floor(Math.random()*(quotes.length))
